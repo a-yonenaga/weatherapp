6843c2f38f097cb0ca428f773a512e1fc257cc97
example <ex@mple.com>
2018-09-14 12:37:49 +0900

    フェーズ4実装

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -5,14 +5,14 @@ require 'date'
 class WelcomeController < ApplicationController
   API_EP = "https://api.darksky.net/forecast/a7daf8b068803135c5f0b7eae0397955/"
 
-  def get_data(lat, lon, type = :daily)
+  def get_data(lat, lon)
     res = Faraday.get "#{API_EP}#{lat},#{lon}", {'units' => 'si'}
-    JSON.parse(res.body)[type.to_s]['data']
+    JSON.parse(res.body)
   end
 
   def get_yest_data(lat, lon)
     time_serial = Time.mktime(Time.now.year, Time.now.month, Time.now.day - 1).to_i
-    res = Faraday.get "#{API_EP}#{lat},#{lon},#{time_serial.to_s}", {'units' => 'si'}
+    res = Faraday.get "#{API_EP}#{lat},#{lon},#{time_serial.to_s}", {'units' => 'si', 'exclue' => 'hourly'}
     res = JSON.parse(res.body)['daily']['data']
     res.each do |rec|
       return rec if rec['time'].to_i == time_serial
@@ -20,33 +20,42 @@ class WelcomeController < ApplicationController
   end
 
   def index
-    @area = 'Tokyo'
-    @point = [35.41, 139.41]
-    data = get_data(*@point,:daily)
+    pref = 0
+    File.open("/home/vagrant/weatherapp/public/pref.json") do |j|
+      pref = JSON.load(j)
+    end
+    @pref_list = pref['marker'].map{|pr| pr['pref']}
 
+    @area = (params[:q] || '12').to_i
+    @area = 12 unless (@area >= 0 && @area <= 46)
+
+    @point = [pref['marker'][@area]['lat'], pref['marker'][@area]['lng']]
+    whole_data = get_data(*@point)
+
+    data = whole_data['daily']['data']
     @list = Array.new(7){|i| Hash.new}
     (0..6).each do |i|
       begin
         @list[i][:icon] = "/icon/#{data[i]['icon']}.png"
-        @list[i][:prec] = "#{(data[i]['precipProbability']*100).to_f}%"
-        @list[i][:max] = "#{data[i]['temperatureHigh']}℃"
-        @list[i][:min] = "#{data[i]['temperatureLow']}℃"
+        @list[i][:prec] = "#{(data[i]['precipProbability']*100).to_f.round(1)}%"
+        @list[i][:max] = "#{data[i]['temperatureHigh'].round(1)}℃"
+        @list[i][:min] = "#{data[i]['temperatureLow'].round(1)}℃"
       rescue
         raise i.to_s + data[i].to_s
       end
     end
 
-    data = get_data(*@point, :hourly)
+    data = whole_data['hourly']['data']
     @graph = Hash.new(8)
     (0..7).each do |i|
-      @graph[Time.at(data[i*6]['time'].to_i).strftime("%d日 %H:00")] = data[i*6]['precipProbability']
+      @graph[Time.at(data[i*6]['time'].to_i).strftime("%d日 %H:00")] = data[i*6]['precipProbability'].round(1)
     end
 
     data = get_yest_data(*@point)
     @yest = Hash.new
     @yest[:icon] = "/icon/#{data['icon']}.png"
-    @yest[:max] = "#{data['temperatureHigh']}℃"
-    @yest[:min] = "#{data['temperatureLow']}℃"
+    @yest[:max] = "#{data['temperatureHigh'].round(1)}℃"
+    @yest[:min] = "#{data['temperatureLow'].round(1)}℃"
 
     render "welcome/index"
   end
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,7 @@ Rails.application.routes.draw do
   get 'welcome/index'
 
   root 'welcome#index'
-  get 'hello' => 'welcome#hello'
-  get 'bye' => 'welcome#bye'
+  get ':q' => 'welcome#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
